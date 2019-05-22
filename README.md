02ee96f819430141788c5320942f41cfac1cb306
CesarTavo <cesartavo@ciencias.unam.mx>
2019-05-22 06:03:49 -0500

    :ok_hand: 3 LAMP's containers and Wordpress

---
+++ README.md
@@ -0,0 +1,10 @@
+# LAMP-3
+
+Here are tree LAMP'S with different containers and haproxy
+
+This branch create tree users that must be administrators to create
+a web content, so if you want to change the names of the users, change it 
+in the script file and docker-compose.yml too 
+
+You could help me by telling me how to make general variables, 
+so there would not be so many changes
---
+++ script.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+USER1=user1
+USER2=user2
+USER3=user3
+USER4=wordpress
+
+sudo useradd -m $USER1
+sudo useradd -m $USER2
+sudo useradd -m $USER3
+sudo useradd -m $USER4
+
+sudo chown -R $(whoami):$(whoami) /home/$USER1
+sudo chown -R $(whoami):$(whoami) /home/$USER2
+sudo chown -R $(whoami):$(whoami) /home/$USER3
+sudo chown -R $(whoami):$(whoami) /home/$USER4
+
+sudo chmod -R 755 /home/$USER1
+sudo chmod -R 755 /home/$USER2
+sudo chmod -R 755 /home/$USER3
+sudo chmod -R 755 /home/$USER4
+
+cp -R ./apache /home/$USER1/apache
+cp -R ./apache /home/$USER2/apache
+cp -R ./apache /home/$USER3/apache
+
+sed -i 's/php:9000/php1:9000/g' /home/$USER1/apache/demo.apache.conf
+sed -i 's/php:9000/php2:9000/g' /home/$USER2/apache/demo.apache.conf
+sed -i 's/php:9000/php3:9000/g' /home/$USER3/apache/demo.apache.conf
+
+cp -R ./mysql /home/$USER1/mysql
+cp -R ./mysql /home/$USER2/mysql
+cp -R ./mysql /home/$USER3/mysql
+
+mkdir /home/$USER1/php
+mkdir /home/$USER2/php
+mkdir /home/$USER3/php
+
+cp -R ./php/local.ini /home/$USER1/php/
+cp -R ./php/local.ini /home/$USER2/php/
+cp -R ./php/local.ini /home/$USER3/php/
+
+cp -R ./public_html/ /home/$USER1/public_html/
+cp -R ./public_html/ /home/$USER2/public_html/
+cp -R ./public_html/ /home/$USER3/public_html/
+
+sed -i "s/\$host = 'mysql'/\$host = 'mysql1'/g" /home/$USER1/public_html/index.php
+sed -i "s/\$host = 'mysql'/\$host = 'mysql2'/g" /home/$USER2/public_html/index.php
+sed -i "s/\$host = 'mysql'/\$host = 'mysql3'/g" /home/$USER3/public_html/index.php
+
+docker-compose build
+docker-compose up -d
+docker restart haproxy
